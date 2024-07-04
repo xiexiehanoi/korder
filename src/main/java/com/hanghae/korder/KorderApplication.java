93b1fcc441fe87540414bd9f80e51afafb5883db
samkim <ksw593vn@gmail.com>
2024-07-04 22:05:05 +0900

    Feat: root 폴더에 src 미사용 및 기본 설정 추가

--- src/main/java/com/hanghae/korder/KorderApplication.java
+++ src/main/java/com/hanghae/korder/KorderApplication.java
@@ -1,16 +1,15 @@
-package com.hanghae.korder;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@SpringBootApplication
-@EnableScheduling
-public class KorderApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(KorderApplication.class, args);
-    }
-
-}
+//package com.hanghae.korder;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//
+//@SpringBootApplication
+//@EnableScheduling
+//public class KorderApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(KorderApplication.class, args);
+//    }
+//
+//}
