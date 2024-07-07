5df46a4f40871a55639935c1a7ebf3a0a91bb187
samkim <ksw593vn@gmail.com>
2024-07-08 00:17:05 +0900

    Feat: 예약 모듈 추가

---
+++ reservation-service/src/main/java/com/hanghae/reservationservice/repository/ReservationRepository.java
@@ -0,0 +1,10 @@
+package com.hanghae.reservationservice.repository;
+
+import com.hanghae.reservationservice.entity.ReservationEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
+
+}
---
+++ reservation-service/src/main/java/com/hanghae/reservationservice/service/ReservationService.java
@@ -0,0 +1,11 @@
+package com.hanghae.reservationservice.service;
+
+import com.hanghae.reservationservice.dto.ReservationDto;
+
+import reactor.core.publisher.Mono;
+
+
+public interface ReservationService {
+    Mono<String> createReservation(ReservationDto dto);
+    Mono<String> confirmReservation(Long reservationId);
+}
